6a805dd26ce658e94df3dac0fbd3608878db0c2b
rep-demo <aashishmijar44@gmail.com>
2025-09-16 22:45:08 +0545

    Updated

--- 01_basics/numbers.py
+++ 01_basics/numbers.py
@@ -1,7 +1,12 @@
-x = 1 # int
+x = 3 # int
 y = 2.3 # float
 z = 4j  # complex
 
+sum = x + y   # should be avoided
+pow = 3 ** 30
 print(type(x))
 print(type(y))
 print(type(z))
+print(sum)
+print(type(sum))
+print(pow)
