a682aff2887248cefa039cdad3c69e311cde2e15
rep-demo <aashishmijar44@gmail.com>
2025-09-17 23:17:50 +0545

    Number related problem

---
+++ 01_basics/Number_Problem.py
@@ -0,0 +1,35 @@
+import random  # For random numbers
+import math
+x = 3 # int
+y = 2.3 # float
+z = 4j  # complex
+
+sum = x + y   # should be avoided
+pow = 3 ** 30
+
+# print(random.randrange(1, 30))
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(sum)  
+# print(type(sum))
+# print(pow)
+
+
+# ----- Octal numbers -----
+oct = 0o20
+print(oct)
+
+hex = 0xFF
+print(hex)
+
+# print(oct(63))
+# print(hex(34))
+print(bin(453))
+
+# -----For octal---
+print(int ('53', 8))
+
+# -----For hexadecimal
+print(int('43', 16))
+
