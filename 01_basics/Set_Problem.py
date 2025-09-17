73899a5ec1634611cdbf5529a8b9912f6e829923
rep-demo <aashishmijar44@gmail.com>
2025-09-17 23:18:50 +0545

    Set related problem

---
+++ 01_basics/Set_Problem.py
@@ -0,0 +1,14 @@
+setOne = {1,3,4,23,54}
+setTwo = {4,45,23,7,4}
+
+# ----- union ----
+print(setOne | setTwo)
+
+# ------ intersection
+print(setOne & setTwo)
+
+# ------ set one only
+print(setOne - setTwo)
+
+# ------ set two only
+print(setTwo - setOne)
