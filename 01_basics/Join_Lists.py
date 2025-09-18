7444fdf5ccd62a68818a604caaa250734ce2bda9
rep-demo <aashishmijar44@gmail.com>
2025-09-18 22:03:29 +0545

    List file added

---
+++ 01_basics/Join_Lists.py
@@ -0,0 +1,16 @@
+list1 = ["a", "b", "c"]
+list2 = [1,2,3]
+
+# list3 = list1+list2
+# print(list3)
+
+# ---- append() method----
+for x in list2:
+    list1.append(x)
+
+# print(list1)
+
+# ----- extend() method, where the purpose is to add elements from one list to another list:
+
+list1.extend(list2)
+print(list1)
