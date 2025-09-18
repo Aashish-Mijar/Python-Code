c8d113cabd1637c09201a08d824a3dcacf8cc06e
rep-demo <aashishmijar44@gmail.com>
2025-09-18 22:08:56 +0545

    Update in Tuple

---
+++ 01_basics/Update_Tuple.py
@@ -0,0 +1,12 @@
+# ------ Converting tuple into a list 
+x = ("apple", "banana", "cherry")
+y = list(x)
+
+# ---- It changes index 1 with kiwi
+# y[1] = "kiwi"
+
+# ---- append --
+y.append("orange")
+y.remove("apple")
+x = tuple(y)
+print(x)
