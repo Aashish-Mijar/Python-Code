b9c2501d6c2efa180fffc3ce974210056389d4ca
rep-demo <aashishmijar44@gmail.com>
2025-09-18 22:08:03 +0545

    Tuple file added

---
+++ 01_basics/Tuple.py
@@ -0,0 +1,20 @@
+# myTuple = ("apple", "banana", "cherry", "apple")
+# print(myTuple)
+# print(len(myTuple))
+# print(type(myTuple))
+
+# NOT a tuple
+# thisTuple = ("apple",)
+thisTuple = ("apple")
+# print(type(thisTuple))
+
+tuple1 = ("Book", "Pencil", "Pen")
+tuple2 = (1,3,4,5,6,7)
+tuple3 = (True, False, False)
+
+# print(tuple1, tuple2, tuple3)
+
+# ------ A tuple with strings, integers and boolean values
+tuple3 = ("Aashish", 34, True, 40, "Male")
+print(tuple3)
+print(type(tuple3))
