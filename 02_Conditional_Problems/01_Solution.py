ff6970c38576427d2aa344bfab3f093c2897adb4
rep-demo <aashishmijar44@gmail.com>
2025-09-19 22:50:50 +0545

    Problem first solution

---
+++ 02_Conditional_Problems/01_Solution.py
@@ -0,0 +1,14 @@
+user_age = int(input("Enter your age:\t"))
+
+if user_age < 13:
+    print("User is child.")
+
+elif user_age >= 13 and user_age <= 19:
+    print("User is Teenager.")
+
+elif user_age > 19 and user_age <= 59:
+    print("User is Adult.")
+
+else:
+    print("User is Senior")
+
