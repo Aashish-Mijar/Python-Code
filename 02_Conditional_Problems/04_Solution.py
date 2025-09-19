94eb7a5e043750ee16ad2a189f277463bcb5f949
rep-demo <aashishmijar44@gmail.com>
2025-09-19 23:23:16 +0545

    Problem four solution

---
+++ 02_Conditional_Problems/04_Solution.py
@@ -0,0 +1,13 @@
+fruit = input("Enter fruit:\t")
+color = input("Enter color of fruit:\t")
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unripe")
+    
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("OverRipe")
+else:
+    print("For now just for Banana...")
