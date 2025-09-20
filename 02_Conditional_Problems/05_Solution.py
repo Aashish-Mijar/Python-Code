42e5c24453b98af2cb515a366d7a33c7f71a729e
rep-demo <aashishmijar44@gmail.com>
2025-09-20 21:39:13 +0545

    Problem 5 solution

---
+++ 02_Conditional_Problems/05_Solution.py
@@ -0,0 +1,12 @@
+weather = input("Enter situation of weather:\t")
+
+if weather == "Sunny":
+    activity = "Go for a walk."
+
+elif weather == "Rainy":
+    activity = "Read a book with a cup of tea."
+
+elif weather == "Snowy":
+    activity = "Build a Snowman."
+
+print(activity)
