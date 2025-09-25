144371b6080bc6b9da51e5050b040b295cba972b
rep-demo <aashishmijar44@gmail.com>
2025-09-25 21:00:16 +0545

    Problem 2 Solution

---
+++ 06_OOP/02_Solution.py
@@ -0,0 +1,11 @@
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+    def full_name(self):
+        return f"Car: {self.brand} Model:{self.model}"
+
+my_Car = Car("Toyota", "Corolla")
+
+print(my_Car.full_name())
