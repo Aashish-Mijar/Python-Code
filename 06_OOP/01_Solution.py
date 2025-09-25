497a0c6a159cb2dcc8434f4450d290df9057a346
rep-demo <aashishmijar44@gmail.com>
2025-09-25 20:59:41 +0545

    Solution 1 modified

--- 06_OOP/01_Solution.py
+++ 06_OOP/01_Solution.py
@@ -1,6 +1,14 @@
 class Car:
-    brand = None
-    model = None
+    # brand = None
+    # model = None
+    
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        
+my_car = Car("Toyota", "Corolla")
+print(my_car.brand)
+print(my_car.model)
 
-my_car = Car()
-print(my_car)
+my_new_car = Car("Tata", "Safari")
+print(my_new_car.brand)
