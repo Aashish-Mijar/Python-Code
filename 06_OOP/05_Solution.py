00e63a19e9911a53a59d24f7a23ac4907a670520
rep-demo <aashishmijar44@gmail.com>
2025-09-25 22:07:01 +0545

    Problem 5 Solution

---
+++ 06_OOP/05_Solution.py
@@ -0,0 +1,31 @@
+class Car:
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.model = model
+
+    def get_brand(self):
+        return self.__brand
+    
+
+    def full_name(self):
+        return f"{self.__brand} {self.model}"
+    
+    def fuel_type(self):
+        return "Petrol or Diesel"
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+    
+    def fuel_type(self):
+        return "Electric Charge"
+
+
+my_tesla = ElectricCar("Bugati", "Model S", "85KWH")
+# print(my_tesla.__brand)
+print(my_tesla.fuel_type())
+
+safari = Car("Tata", "Safari")
+print(safari.fuel_type())
+
