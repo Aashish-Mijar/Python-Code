25474afa86628d5bafac0fa2131265bdd9999550
rep-demo <aashishmijar44@gmail.com>
2025-09-28 21:17:30 +0545

    File modified

--- 06_OOP/07_Solution.py
+++ 06_OOP/07_Solution.py
@@ -10,6 +10,11 @@ class Car:
 
     def full_name(self):
         return f"{self.__brand} {self.model}"
+    
+    @staticmethod   #--- Decorators
+    def general_description():
+        return "Cars are means of transpotation"
+
 
 class ElectricCar(Car):
     def __init__(self, brand, model, battery_size):
@@ -18,6 +23,10 @@ class ElectricCar(Car):
 
 
 my_tesla = ElectricCar("Bugati", "Model S", "85KWH")
+my_Car = Car("Tata", "Nexon")
 # print(my_tesla.__brand)
-print(my_tesla.get_brand())
-print(my_tesla.full_name())
+# print(my_tesla.get_brand())
+# print(my_tesla.full_name())
+
+# print(my_Car.general_description())
+print(Car.general_description())
