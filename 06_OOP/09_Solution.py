e3a859bf617ff3b2817bbe8e68883a7e53e920ee
rep-demo <aashishmijar44@gmail.com>
2025-09-28 21:30:48 +0545

    Problem 9 Solution

---
+++ 06_OOP/09_Solution.py
@@ -0,0 +1,44 @@
+# --- Encapsulation----
+class Car:
+    def __init__(self, brand, model):
+        self.__brand = brand
+        self.__model = model
+
+    def get_brand(self):
+        return self.__brand
+    
+
+    def full_name(self):
+        return f"{self.__brand} {self.__model}"
+    
+    @staticmethod   #--- Decorators
+    def general_description():
+        return "Cars are means of transpotation"
+    
+    @property
+    def model(self):
+        return self.__model
+
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+
+my_tesla = ElectricCar("Bugati", "Model S", "85KWH")
+
+print(isinstance(my_tesla, Car))
+print(isinstance(my_tesla, ElectricCar))
+
+
+# my_Car = Car("Tata", "Nexon")
+# my_Car.model = "City"
+# print(my_tesla.__brand)
+# print(my_tesla.get_brand())
+# print(my_tesla.full_name())
+
+# print(my_Car.general_description())
+# print(Car.general_description())
+
+# print(my_Car.model)
