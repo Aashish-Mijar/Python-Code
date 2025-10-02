3744a03205565ba159b7f174c606cb89329a85f9
rep-demo <aashishmijar44@gmail.com>
2025-10-02 20:45:52 +0545

    Changes made in Rent_Calc

--- 13_Projects/Rent_Calc/main.py
+++ 13_Projects/Rent_Calc/main.py
@@ -6,3 +6,7 @@ charge_per_unit = int(input("Enter the charge per unit is = "))
 persons = int(input("Enter the number of persons living in room/flat = "))
 
 total_bill = electricity_spend * charge_per_unit 
+
+output = (food + rent + total_bill) // persons
+
+print("Each person will pay = ", output)
