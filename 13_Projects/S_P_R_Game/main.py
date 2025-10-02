0293a74fdbbddb9c622dbeebcac5dbeaee423d06
rep-demo <aashishmijar44@gmail.com>
2025-10-02 21:01:21 +0545

    Conditions added

--- 13_Projects/S_P_R_Game/main.py
+++ 13_Projects/S_P_R_Game/main.py
@@ -4,3 +4,19 @@ user_choice = input("Enter your move (Rock, Paper, Scissor): ")
 computer_choice = random.choice(item_list)
 
 print(f"User choice = {user_choice}, Computer choice = {computer_choice}")
+
+if user_choice == computer_choice:
+    print("Both user chose same!")
+    print("Match tie")
+
+elif user_choice == "Rock":
+    if computer_choice == "Paper":
+        print("Paper covers Rock!")
+        print("Computer wins!")
+    
+    else:
+        print("Rock breaks Scissor!")
+        print("You win!")
+
+
+    
