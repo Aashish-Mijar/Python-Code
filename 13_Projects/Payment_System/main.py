d5b7b0a994d03773a12213d33c06bb3b92a86d26
rep-demo <aashishmijar44@gmail.com>
2025-10-04 19:33:19 +0545

    Generate qr code feature for payment

--- 13_Projects/Payment_System/main.py
+++ 13_Projects/Payment_System/main.py
@@ -12,3 +12,7 @@ phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
 paytm_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
 google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
 
+# Create QR Codes for each payment app
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
