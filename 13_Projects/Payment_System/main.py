756328b4f04bfdb95c0165e4fc0a38bf4156721a
rep-demo <aashishmijar44@gmail.com>
2025-10-04 19:35:38 +0545

    Adding functionility to save qr codes

--- 13_Projects/Payment_System/main.py
+++ 13_Projects/Payment_System/main.py
@@ -16,3 +16,8 @@ google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
 phonepe_qr = qrcode.make(phonepe_url)
 paytm_qr = qrcode.make(paytm_url)
 google_pay_qr = qrcode.make(google_pay_url)
+
+# Save the QR code to image file
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
