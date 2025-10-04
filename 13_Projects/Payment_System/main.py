db55657b8068cc24422cb2a08a3657c27badef6f
rep-demo <aashishmijar44@gmail.com>
2025-10-04 19:31:20 +0545

    url for payment method added

--- 13_Projects/Payment_System/main.py
+++ 13_Projects/Payment_System/main.py
@@ -4,3 +4,11 @@ import qrcode
 upi_id = input("Enter your UPI ID = ")
 
 # upi://pay?pa=UPI_ID&apn=NAME&am=Amount&cu=CURRENCY&tn=MESSAGE
+
+# Defining the payment URL based on the UPI ID and the payment app
+# Can be modified these URLs based on the payment apps 
+
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
