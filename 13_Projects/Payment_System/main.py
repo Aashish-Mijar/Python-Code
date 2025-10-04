885db57abf718228fcb233dc8969193a014dfc6b
rep-demo <aashishmijar44@gmail.com>
2025-10-04 19:46:18 +0545

    Final commit on Payment_system

--- 13_Projects/Payment_System/main.py
+++ 13_Projects/Payment_System/main.py
@@ -1,28 +1,29 @@
 import qrcode
 
-# Taking UPI ID As a input
-upi_id = input("Enter your UPI ID = ")
+# Taking Wallet ID As a input
+wallet_id = input("Enter your Wallet ID / Mobile Number = ")
 
-# upi://pay?pa=UPI_ID&apn=NAME&am=Amount&cu=CURRENCY&tn=MESSAGE
+# custom payment message
+amount = input("Enter Amount (optional, press Enter to skip): ")
+remarks = input("Enter Remakrs / Message (optional, press Enter to skip): ")
 
-# Defining the payment URL based on the UPI ID and the payment app
+# Defining the payment URL based on the Wallet ID / Mobile Number and the payment app
 # Can be modified these URLs based on the payment apps 
-
-phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
-paytm_url=f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
-google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+esewa_url = f'https://esewa.com.np/qr?sc=esewa&pa={wallet_id}&am={amount}&tn={remarks}'
+khalti_url = f'https://khalti.com/pay?pa={wallet_id}&am={amount}&tn={remarks}'
+imepay_url = f'https://imepay.com/pay?pa={wallet_id}&am={amount}&tn={remarks}'
 
 # Create QR Codes for each payment app
-phonepe_qr = qrcode.make(phonepe_url)
-paytm_qr = qrcode.make(paytm_url)
-google_pay_qr = qrcode.make(google_pay_url)
+esewa_qr = qrcode.make(esewa_url)
+khalti_qr = qrcode.make(khalti_url)
+imepay_qr = qrcode.make(imepay_url)
 
 # Save the QR code to image file
-phonepe_qr.save('phonepe_qr.png')
-paytm_qr.save('paytm_qr.png')
-google_pay_qr.save('google_pay_qr.png')
+esewa_qr.save('esewa_qr.png')
+khalti_qr.save('khalti_qr.png')
+imepay_qr.save('imepay_qr.png')
 
 # Display the  qr codes (you may need to install pIL/pillow library)
-phonepe_qr.show()
-paytm_qr.show()
-google_pay_qr.show()
+esewa_qr.show()
+khalti_qr.show()
+imepay_qr.show()
