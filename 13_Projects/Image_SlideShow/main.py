494628f4b96bbe35d9a1044ace9280454085ee1e
rep-demo <aashishmijar44@gmail.com>
2025-10-06 11:00:01 +0545

    Accessing Images to be slide showed

--- 13_Projects/Image_SlideShow/main.py
+++ 13_Projects/Image_SlideShow/main.py
@@ -3,3 +3,17 @@ from PIL import Image, ImageTk
 import time
 import tkinter as tk
 
+root = tk.Tk()
+root.title("Image Slideshow viewer")
+
+# List of Image Path
+image_path = [
+    r'\For_SlideShow_app\img1.jpg',
+    r'\For_SlideShow_app\img2.jpg',
+    r'\For_SlideShow_app\img3.jpg',
+    r'\For_SlideShow_app\img4.jpg',
+    r'\For_SlideShow_app\img5.jpg',
+    r'\For_SlideShow_app\img6.jpg',
+]
+
+# Resize the images to 1080 x 1080
