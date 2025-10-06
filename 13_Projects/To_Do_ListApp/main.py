aaf7824bfc42f45102b8b0670474c285f1027987
rep-demo <aashishmijar44@gmail.com>
2025-10-06 16:25:00 +0545

    Creating Console to do list app

---
+++ 13_Projects/To_Do_ListApp/main.py
@@ -0,0 +1,8 @@
+def task():
+    tasks = []
+    print("-----WELCOME TO THE TASK MANAGEMENT APP----")
+
+    total_task = int(input("Enter how many task you want to add = "))
+    for i in range(1, total_task+1):
+        task_name = input(f"Enter task {i}= ")
+        tasks.append(task_name)
