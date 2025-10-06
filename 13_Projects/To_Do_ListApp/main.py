80ff43ccefcdc83eb2df0786018d6ec03c9ea865
rep-demo <aashishmijar44@gmail.com>
2025-10-06 16:28:48 +0545

    Looping through every task

--- 13_Projects/To_Do_ListApp/main.py
+++ 13_Projects/To_Do_ListApp/main.py
@@ -6,3 +6,10 @@ def task():
     for i in range(1, total_task+1):
         task_name = input(f"Enter task {i}= ")
         tasks.append(task_name)
+    
+    print(f"Today's tasks are\n{tasks}")
+    while True:
+        operation = int(input("Enter 1-Add\n2-Update\n3-Delete\n4-View\n5-Exit/Stop"))
+        add = input("Enter task you want to add = ")
+        tasks.append(add)
+        print(f"Task {add} has been successfully added...")
