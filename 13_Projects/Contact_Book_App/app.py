053109f2c2c99655c44fd592cd70e5a2a2e0e4ac
rep-demo <aashishmijar44@gmail.com>
2025-10-18 19:56:40 +0545

    First commit

---
+++ 13_Projects/Contact_Book_App/app.py
@@ -0,0 +1,17 @@
+# empty dictionary
+contacts = {}
+
+while True:
+    print("\nContact Book App")
+    print('1. Create Contact')
+    print('2. View Contact')
+    print('3. UPdate Contact')
+    print('4. Delete Contact')
+    print('5. Search Contact')
+    print('6. Count Contact')
+    print('7. Exit ')
+
+    choice = input("Enter your choice = ")
+
+    if choice == '1':
+        
