f7b051de53dd47924e5bcff3bc655f2f7dc76717
rep-demo <aashishmijar44@gmail.com>
2025-10-19 19:56:59 +0545

    Second commit

--- 13_Projects/Spell_checker/app.py
+++ 13_Projects/Spell_checker/app.py
@@ -10,3 +10,9 @@ class SpellCheckerApp:
         words = text.split()
         corrected_words = []
 
+        for word in words:
+            corrected_word = self.spell.correction(word)
+            if corrected_word != word.lower():
+                print(f'Correcting "{word}" to "{corrected_word}"')
+                corrected_words.append(corrected_word)
+
