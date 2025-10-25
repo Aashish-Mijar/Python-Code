f3efc0b2a29b986a7ae3597ebff5a38ffa46c035
rep-demo <aashishmijar44@gmail.com>
2025-10-25 18:39:21 +0545

    Using f-String formatting

--- 01_basics/Advance_String.py
+++ 01_basics/Advance_String.py
@@ -6,3 +6,8 @@ print(text.strip().lower())
 # Count and replace
 print("Count of 'o':", text.count('o'))
 print(text.replace("Awesome", "Powerful"))
+
+# Using f-String formatting
+name = "Aashish"
+marks = 90
+print(f"{name:^10} | {marks:>3}")
