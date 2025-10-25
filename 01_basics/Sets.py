660baad173dabeff845d79c9ce45c0cfea74c141
rep-demo <aashishmijar44@gmail.com>
2025-10-25 18:33:24 +0545

    Removing duplicates

--- 01_basics/Sets.py
+++ 01_basics/Sets.py
@@ -1,7 +1,11 @@
-A = {1, 3, 5,7}
-B = {2,3,4,6,5}
+A = {1, 2,3,4}
+B = {3,4,5,6}
 
 print("Union: ", A|B)
 print("Intersection: ", A&B)
 print("Difference (A-B): ", A-B)
 print("Symmetric Difference: ", A^B)
+
+# Removing duplicates using sets
+
+data = [1,2,2,3,4,4,5]
