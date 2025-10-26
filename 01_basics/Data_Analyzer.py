f6f72c37c572ffe80d146b226f72c2ffb84ee387
rep-demo <aashishmijar44@gmail.com>
2025-10-26 19:09:58 +0545

    Data 3

--- 01_basics/Data_Analyzer.py
+++ 01_basics/Data_Analyzer.py
@@ -1,4 +1,5 @@
 students={
     "Mira": {"math": 88, "Science": 92, "english":85},
     "Aashish": {"math":99, "Science": 96, "english": 98},
+    "Rina": {"math": 78, "Science":80, "english": 85}
 }
