3302d167df03c4b6c385a09cf67998c83ebba4da
rep-demo <aashishmijar44@gmail.com>
2025-10-28 07:35:10 +0545

    Adding comments

--- 01_basics/Remove_Duplicates.py
+++ 01_basics/Remove_Duplicates.py
@@ -1,4 +1,6 @@
+# Program to remove duplicate and sort it
 numbers = [4, 1,2,4,3,2,5,1]
 
+# using sorting function
 unique_sorted = sorted(set(numbers))
 print("Unique sorted numbers: ", unique_sorted)
