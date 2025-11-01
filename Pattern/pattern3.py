7049932661febb16294c3e83fbebb2de4a9f47f8
rep-demo <aashishmijar44@gmail.com>
2025-11-01 19:58:11 +0545

    Final commit

--- Pattern/pattern3.py
+++ Pattern/pattern3.py
@@ -2,3 +2,9 @@ import os
 import time
 
 n = 5
+for k in range(20):  # Number of moves
+    os.system('cls')
+    print("\n" * (k % 10))  # moves down
+    for i in range(1, n + 1):
+        print(" " * (n - i) + "*" * (2 * i - 1))
+    time.sleep(0.2)
