f97caf04ef708da5d94a0509222b440f79103b11
rep-demo <aashishmijar44@gmail.com>
2025-11-01 20:00:10 +0545

    Final commit on rotating line

--- Pattern/pattern4.py
+++ Pattern/pattern4.py
@@ -1,4 +1,9 @@
 import time
 import sys
 
-
+spinner = ['|', '/', '-', '\\']
+for _ in range(20):
+    for frame in spinner:
+        sys.stdout.write('\r' + frame)
+        sys.stdout.flush()
+        time.sleep(0.1)
