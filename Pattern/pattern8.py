23a75360093bf6e0ae8945ab407f907d0f679feb
rep-demo <aashishmijar44@gmail.com>
2025-11-03 19:00:09 +0545

    Rotating Bar

--- Pattern/pattern8.py
+++ Pattern/pattern8.py
@@ -1,2 +1,8 @@
 import sys, time
 
+spinner = ['|', '/', '-', '\\']
+for _ in range(50):
+    for frame in spinner:
+        sys.stdout.write('\rLoading... ' + frame)
+        sys.stdout.flush()
+        time.sleep(0.1)
