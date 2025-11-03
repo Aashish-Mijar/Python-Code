ad1cb319d78ed542fcbe7994d00e40e98f2d6ac4
rep-demo <aashishmijar44@gmail.com>
2025-11-03 18:58:36 +0545

    Final commit

--- Pattern/pattern7.py
+++ Pattern/pattern7.py
@@ -1,3 +1,8 @@
 import os, time, math
 
-
+for t in range(200):
+    os.system('cls')
+    for x in range(60):
+        y = int(10 + 5 * math.sin((x + t) * 0.3))
+        print(" " * y + "*")
+    time.sleep(0.05)
