15085ed798c60e0d6a5b279919f75958bd2ce2e1
rep-demo <aashishmijar44@gmail.com>
2025-11-03 18:56:44 +0545

    Final commit

--- Pattern/pattern6.py
+++ Pattern/pattern6.py
@@ -4,3 +4,10 @@ width = 40
 pos = 0
 direction = 1
 
+while True:
+    os.system('cls')
+    print(" " * pos + "O")  # The ball
+    time.sleep(0.05)
+    pos += direction
+    if pos == 0 or pos == width:
+        direction *= -1
